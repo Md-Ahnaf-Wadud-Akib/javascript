1891d5812471e998f9c70e02a97ea0aeb9e674c5
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-10 08:16:09 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,11 @@
+// console.log(77);
+
+// var name = "Ahanf Wadud Akib";
+// console.log(name);
+
+var eggPrice, onionPrice, totalPrice;
+
+eggPrice = 10;
+onionPrice = 12;
+totalPrice = eggPrice + onionPrice;
+console.log(totalPrice);
---
+++ variable.js
@@ -0,0 +1,24 @@
+var name = "Ahanf Akib";
+// console.log(name);
+
+var price1, price2, add, sub, multi, divition;
+price1 = 10;
+price2 = 5;
+add = price1 + price2;
+sub = price1 - price2;
+multi = price1 * price2;
+divition = price1 / price2;
+
+// console.log(add);
+// console.log(sub);
+// console.log(multi);
+// console.log(divition);
+
+price1 += 10;
+// console.log(price1);
+
+price1 -= 2;
+// console.log(price1);
+
+price1++;
+console.log(price1);
