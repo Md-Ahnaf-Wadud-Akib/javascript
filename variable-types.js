103eaca20e2cf74eb2459cd1fe055055cc20a200
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-10 09:17:54 +0600

    types of variable

---
+++ variable-types.js
@@ -0,0 +1,12 @@
+var value1, value2, value3;
+value1 = 10;
+value2 = "Ahnaf";
+// console.log(typeof value1)
+// console.log(typeof value2)
+// console.log(typeof value3)
+var number1, number2, sum;
+number1 = 0.1;
+number2 = 0.2;
+sum = number1 + number2;
+
+console.log(parseFloat(sum.toFixed(1))) 
