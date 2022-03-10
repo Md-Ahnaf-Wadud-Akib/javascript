7f5f3b9c23d8d7d3edb2294fc6bdb90126a71b35
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-10 08:50:55 +0600

    conversion

---
+++ conversion.js
@@ -0,0 +1,18 @@
+var firstName, lastName, fullName;
+firstName = "Ahanf";
+lastName = "Akib";
+fullName = firstName + " " + lastName;
+// console.log(fullName)
+
+var firstNumber, secondNumber, sum, sumConversion;
+firstNumber = "14.576";
+secondNumber = "23";
+
+sum = firstNumber + " " + secondNumber;
+console.log(sum)
+
+// convertiong the "number string" in to a "number"
+firstNumberConversion = parseFloat(firstNumber);
+secondNumberConversion = parseInt(secondNumber);
+sumConversion = firstNumberConversion + secondNumberConversion;
+console.log(sumConversion)
