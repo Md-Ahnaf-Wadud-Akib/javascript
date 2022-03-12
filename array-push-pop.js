7844994a283e92ccb55aa6fbcd518e29db75fe1c
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-12 06:48:37 +0600

    push-pop

---
+++ array-push-pop.js
@@ -0,0 +1,10 @@
+// array push
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+number.push(10);
+number.push(11);
+console.log(number);
+
+// array pop
+var string = ["Ahanf", "Akib", "Saif"];
+string.pop();
+console.log(string);
