9a93b2c8243b8f8c5e1f4b0820f86e4538fadd5d
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-12 07:30:15 +0600

    if-else

--- if-else.js
+++ if-else.js
@@ -1,9 +1,24 @@
-var money = 120;
-var foodPrice = 115;
+// var money = 120;
+// var foodPrice = 115;
 
-if (foodPrice > money) {
-    console.log("no food");
+// if (foodPrice > money) {
+//     console.log("no food");
+// }
+// else {
+//     console.log("food is searved");
+// }
+
+var money, job, flat;
+money = false;
+job = true;
+flat = false;
+
+if (job == true && money == true && flat == true) {
+    console.log("get married");
+}
+else if (job == true && (money == false || flat == true)) {
+    console.log("get married");
 }
 else {
-    console.log("food is searved");
+    console.log("not getting married")
 }
