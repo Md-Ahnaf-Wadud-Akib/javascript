585bebc92660697e91ca08f66037a8d59d5f37fa
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-12 06:06:44 +0600

    array

--- variable-types.js
+++ variable-types.js
@@ -9,10 +9,18 @@ number1 = 0.1;
 number2 = 0.2;
 sum = number1 + number2;
 
-console.log(parseFloat(sum.toFixed(1)))
+// console.log(parseFloat(sum.toFixed(1)))
 
 // modulus 
 var mango = 13;
 var eater = 5;
 var remaining = mango % eater;
-console.log(remaining)
+// console.log(remaining)
+
+// Array:
+var name = ["Ahnaf", "Akib"];
+// console.log(name)
+
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(number)
+console.log(number.length)
