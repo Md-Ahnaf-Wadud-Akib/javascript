eca3fb142e6a17fcf1c5c91e12dc3fdfb6efaf01
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-12 07:50:43 +0600

    if-else

--- if-else.js
+++ if-else.js
@@ -8,17 +8,37 @@
 //     console.log("food is searved");
 // }
 
-var money, job, flat;
-money = false;
-job = true;
-flat = false;
+// var money, job, flat;
+// money = false;
+// job = true;
+// flat = false;
 
-if (job == true && money == true && flat == true) {
-    console.log("get married");
+// if (job == true && money == true && flat == true) {
+//     console.log("get married");
+// }
+// else if (job == true && (money == false || flat == true)) {
+//     console.log("get married");
+// }
+// else {
+//     console.log("not getting married")
+// }
+
+var myMoney = 10;
+var barger = 60;
+var porota = 25;
+var cha = 5;
+milk = false;
+if (myMoney > barger) {
+    console.log("barger khabo");
 }
-else if (job == true && (money == false || flat == true)) {
-    console.log("get married");
+else if (myMoney > porota) {
+    console.log("porota khabo");
 }
 else {
-    console.log("not getting married")
+    if (milk == true) {
+        console.log("dudu cha khabo");
+    }
+    else {
+        console.log("normal cha khabo");
+    }
 }
