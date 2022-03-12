46c98cf4185c272af7a7b569d75917a24f77d639
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-03-12 11:09:55 +0600

    loop

--- module-17.5/while-loop.js
+++ module-17.5/while-loop.js
@@ -1,6 +1,14 @@
-var money = 9;
+var money = 2;
 
-while (money < 10) {
-    console.log("get a candy");
-    money++;
+// while (money < 10) {
+//     console.log("get a candy");
+//     money++;
+//     console.log(money);
+// }
+
+var number = 1;
+
+while (number <= 20) {
+    console.log(number);
+    number = number + 2;
 }
