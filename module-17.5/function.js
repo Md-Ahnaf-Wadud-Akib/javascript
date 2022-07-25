22c1d4cc3dae3e945e675d795612b0613aab71e0
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2022-07-25 15:07:32 +0600

    function

---
+++ module-17.5/function.js
@@ -0,0 +1,21 @@
+// function mileToKilometer(mile) {
+//     var kilometer = mile * 1.60934;
+//     return kilometer;
+// }
+
+// var inputMile = 5;
+
+// var result = mileToKilometer(inputMile);
+// console.log(result);
+
+function evenNumber(num) {
+    if (num % 2 == 0) {
+        console.log("the value is even.");
+    }
+    else {
+        console.log("the number is not even.");
+    }
+}
+
+var number = 134;
+var result = evenNumber(number);
